f8328766a2ec77636afda1824388804c2f1860a9
TonyBrb <anthonybarbagallo95@gmail.com>
2022-01-17 15:18:16 +0100

    creazione classe, istanze, costruttore, metodo e stampa a video

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+class Movie
+{
+  public $title;
+  public $genre;
+  public $year;
+  public $vote;
+
+  function __construct($_title)
+  {
+    $this->title = $_title;
+  }
+
+  public function getVote(){
+    $valutation = 'good';
+    if($this->vote < 5) $valutation = 'low';
+    else if($this->vote < 7) $valutation = 'medium';
+    return $valutation;
+  }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-oop-1</title>
+</head>
+
+<body>
+
+  <?php
+  $fast_and_furious = new Movie($_fast_and_furious);
+  $fast_and_furious->title = "Fast and Fuorious Tokio Drift";
+  $fast_and_furious->genre = "Azione";
+  $fast_and_furious->year = 2006;
+  $fast_and_furious->vote = 5;
+
+  $interstellar = new Movie($_interstellar);
+  $interstellar->title = "Interstellar";
+  $interstellar->genre = "Avventura/Fantascienza";
+  $interstellar->year = 2014;
+  $interstellar->vote = 9;
+  ?>
+  <h1>Titolo:</h1>
+  <h3><?php echo $fast_and_furious->title ?></h3>
+  <h1>Genere:</h1>
+  <h3><?php echo $fast_and_furious->genre ?></h3>
+  <h1>Anno:</h1>
+  <h3><?php echo $fast_and_furious->year ?></h3>
+  <h1>Voto:</h1>
+  <h3><?php echo $fast_and_furious->getVote(); ?></h3>
+
+
+  <h1>Titolo:</h1>
+  <h3><?php echo $interstellar->title ?></h3>
+  <h1>Genere:</h1>
+  <h3><?php echo $interstellar->genre ?></h3>
+  <h1>Anno:</h1>
+  <h3><?php echo $interstellar->year ?></h3>
+  <h1>Voto:</h1>
+  <h3><?php echo $interstellar->getVote(); ?></h3>
+</body>
+
+</html>
